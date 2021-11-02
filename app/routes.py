b316c0f327f0fd1db5d75df2bcd2df501cdd1dee
Megan <megan.hagar@gmail.com>
2021-11-02 12:55:38 -0700

    readying for deployment

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,74 @@
-from flask import Blueprint
+from app import db
+from app.models.book import Book
+from flask import Blueprint, jsonify, make_response, request
 
+hagantine_library = Blueprint("hagantine", __name__, url_prefix="/books")
+
+
+# books = [
+#     Book(1, "The Marvelous Mrs. Gaysel", "Spunky, New York, and undeniably gay."),
+#     Book(2, "Frankgay & Gayce", "Turns out Sal and Robert aren't alone at Club Dorothy."),
+#     Book(3, "Gayousts", "A couple inherits a rundown mansion in rural Greenwich Village\
+#  only to discover they share ownership with some truly queer neighbors")
+# ]
+
+@hagantine_library.route("", methods=["GET", "POST"])
+def handle_books():
+    if request.method == "GET":
+        title_query = request.args.get("title")
+        if title_query:
+            books = Book.query.filter_by(title=title_query)
+        else:
+            books = Book.query.all()
+        books_response = []
+        for book in books:
+            books_response.append({
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            })
+        return jsonify(books_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_book = Book(title=request_body["title"], 
+                        description=request_body["description"])
+
+        db.session.add(new_book)
+        db.session.commit()
+
+        return make_response(f"Book {new_book.title} successfully created, with id {new_book.id}", 201)
+
+@hagantine_library.route("/<id>", methods=["GET"])
+def handle_book(id):
+    book = Book.query.get(id)
+    if book:
+        return jsonify({"id": book.id,
+                    "title": book.title,
+                    "description": book.description
+                })
+    else:
+        return make_response(f"Your book id #{id} was not found", 404)
+
+@hagantine_library.route("/<id>", methods=["PUT"])
+def update_a_book(id):
+    book = Book.query.get(id)
+    request_body = request.get_json()
+
+    if book:
+        book.title = request_body["title"]
+        book.description = request_body["description"]
+        db.session.commit()
+        return make_response(f"{book.title} was successfully updated with {book.description=}")
+    else:
+        title = request_body["title"]
+        return make_response(f"Your book id #{id} was not found and {title} could not be updated", 404)
+
+@hagantine_library.route("/<id>", methods=["DELETE"])
+def delete_a_book(id):
+    book = Book.query.get(id)
+    if book: 
+        db.session.delete(book)
+        db.session.commit()
+        return make_response(f"Book #{book.id} was successfully deleted")
+    else:
+        return make_response(f"Your book id #{id} was not found and could not be deleted", 404)
---
+++ tests/test_routes.py
@@ -0,0 +1,44 @@
+import json
+
+def test_get_all_books_with_no_records(client):
+    # Act
+    response = client.get("/books")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
+
+def test_get_one_book(client, two_saved_books):
+    # Act
+    response = client.get("/books/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "title": "Ocean Book",
+        "description": "watr 4evr"
+    }
+
+def test_get_all_book(client, two_saved_books):
+    # Act
+    response = client.get("/books")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 2
+
+def test_post_new_book(client):
+    # Act
+    data = {
+        "title": "Prairie Book", 
+        "description": "i lurve the grass" 
+    }
+    response = client.post("/books", json = data)
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 201
